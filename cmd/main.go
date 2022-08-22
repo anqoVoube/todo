99f64422fd1bdfc2ac38e231761d43e2bfb33782
anqoVoube <91607120+anqoVoube@users.noreply.github.com>
2022-08-22 09:18:52 +0500

    First commit

---
+++ cmd/main.go
@@ -0,0 +1,49 @@
+package main
+
+import (
+	"github.com/anqovoube/todo-app"
+	"github.com/anqovoube/todo-app/pkg/handler"
+	"github.com/anqovoube/todo-app/pkg/repository"
+	"github.com/anqovoube/todo-app/pkg/service"
+	"github.com/joho/godotenv"
+	_ "github.com/lib/pq"
+	"github.com/spf13/viper"
+	"log"
+	"os"
+)
+
+func main() {
+	if err := initConfig(); err != nil {
+		log.Fatalf("error occurred while trying to initialize config: %s", err.Error())
+	}
+
+	if err := godotenv.Load(); err != nil {
+		log.Fatalf("error loading env variables: %s", err.Error())
+	}
+
+	db, err := repository.NewPostgresDB(repository.Config{
+		Host:     viper.GetString("db.host"),
+		Port:     viper.GetString("db.port"),
+		Username: viper.GetString("db.username"),
+		Password: viper.GetString("db.password"),
+		DBName:   viper.GetString("db.dbname"),
+		SSLMode:  os.Getenv("DB_PASSWORD"),
+	})
+	if err != nil {
+		log.Fatalf("failed to initialize db: %s", err.Error())
+	}
+	repos := repository.NewRepository(db)
+	services := service.NewService(repos)
+	handlers := handler.NewHandler(services)
+
+	srv := new(todo.Server)
+	if err := srv.Run(viper.GetString("port"), handlers.InitRoutes()); err != nil {
+		log.Fatalf("error occurred while running http server: %s", err.Error())
+	}
+}
+
+func initConfig() error {
+	viper.AddConfigPath("configs")
+	viper.SetConfigName("config")
+	return viper.ReadInConfig()
+}
